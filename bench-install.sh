baad80c5cca96b3cfb2d19fbe2afdee1ae139d4c
amol <amol909@gmail.com>
2024-08-07 22:15:47 +0530

    First commit

--- bench-install.sh
+++ bench-install.sh
@@ -4,8 +4,8 @@ if (($EUID != 0)); then
   if [[ -t 1 ]]; then
     sudo "$0" "$@" 
   else
-    exec 1>output_file &&
-    gksu "$0 $@" &&
+    exec 1>output_file 
+    gksu "$0 $@" 
   fi
   exit
 fi
