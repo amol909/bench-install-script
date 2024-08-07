51f3dd59e86aba66143a8d55d79d21d6bd6ced68
amol <amol909@gmail.com>
2024-08-07 22:53:52 +0530

    First commit

--- bench-install.sh
+++ bench-install.sh
@@ -76,6 +76,9 @@ curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash &
 
 source ~/.profile || source ~/.bashrc &&
 
+export NVM_DIR="$HOME/.nvm" &&
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" &&
 
 nvm install 18 &&
 
